1bedba2c960a20a76ba651a8ce5ca239366005ee
Rafael E Quintero <raf4.quintero@gmail.com>
2022-05-12 20:12:17 -0500

    added counter components

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,21 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState } from "react";
+import Counter from "./components/counter/counter";
+import Button from "./components/button/button";
+// import logo from './logo.svg'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+      <Counter />
+      <div style={{ display: "flex" }}>
+        <Button label="Incrementar" />
+        <Button label="Decrementar" />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/counter/counter.jsx
@@ -0,0 +1,5 @@
+const Counter = ({ counter }) => {
+  return <h1>{counter}</h1>;
+};
+
+export default Counter;
